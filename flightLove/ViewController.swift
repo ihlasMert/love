3ec8923fed25f49f44d6867d1cb22255f9c12cac
İhlas Mert <ihlas1mert@gmail.com>
2021-11-03 12:09:10 +0300

    Initial Commit

---
+++ flightLove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flightLove
+//
+//  Created by ihlas on 3.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
